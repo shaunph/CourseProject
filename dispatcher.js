6db21d0453f33f4aad2fecbc15a933237399c286
Jeremy Jackins <jnjackin@ucalgary.ca>
2011-02-10 18:50:52 -0700

    Begins implementing a dispatcher / exception handler. Creates some stubs to work with (such as a main page placeholder), basic 404 and 500 error pages, and responds with a  404 if the requested object is not found. For now uses naive cases since there is no  database yet to work with.

---
+++ dispatcher.js
@@ -0,0 +1,82 @@
+var http = require('http'),
+    url = require('url'),
+    util = require('util'),
+    path = require('path'),
+    fs = require('fs'),
+    exec = require('child_process').exec;
+
+var port = 5678;
+var page_root = "html",
+    img_root = "html/img",
+    error_root = "html/error_pages";
+
+function error_404(response, filename)
+{
+	util.log("\tERROR: 404: " + filename);
+	response.writeHead(404, {"Content-Type": "text/html"});
+	util.pump(fs.createReadStream(error_root + "/404.html"), response, function(){});
+}
+
+function error_500(response)
+{
+	util.log("\tERROR: 500");
+	response.writeHead(500, {"Content-Type": "text/html"});
+	util.pump(fs.createReadStream(error_root + "/500.html"), response, function(){});
+}
+
+/* Upon receiving a request, try to match it with a response object. If
+   no corresponding object is found, respond with a 404 error page. In case
+   an unresolvable exception is encountered, repond with a 500 error page. */
+function resolve(req, res) {
+    pathname = url.parse(req.url).pathname;
+    util.log("request: " + pathname);
+
+    /* presumably the requested path will be searched for in a database, for now
+       let's use a simple switch statement */
+    switch(pathname) {
+	case "/":
+	    send_obj(res, "/main.html", "text/html");
+	    break;
+	case "/test.jpg":
+	    send_obj(res, "/test.jpg", "image/jpeg");
+	    break;
+
+	/* reported to exist, but doesn't actually exist */
+        case "/server_error":
+	    send_obj(res, "does_not_exist", "text/html");
+	    break;
+	    
+	/* Nothing was found, 404 */
+        default:
+	    error_404(res, pathname);
+	    break;
+    }
+}
+
+function send_obj(response, filename, type) {
+    var fullpath;
+    if (type == "text/html") {
+	    fullpath = page_root + filename;
+    } else if (type == "image/jpeg") {
+	    fullpath = img_root + filename;
+    } else {
+	    error_500(response);
+    }
+    path.exists(fullpath, function(exists) {
+       if (exists) {
+	   util.log("\tSending file: " + fullpath);
+           response.writeHead(200, {'Content-Type': type});
+           util.pump(fs.createReadStream(fullpath), response, function() {});
+       } else {
+	   error_500(response);
+       }
+    });
+}
+
+/* For now, use some unpriviledged port */
+http.createServer(resolve).listen(port);
+
+/* Intelligently report the hostname and port. Assumes we are using a UNIX environment. */
+exec("uname -n", function(error, stdout, stderr) {
+	util.log("Server running at " + stdout.slice(0, stdout.length-1) + ":" + port);
+});
