af4b1fed6af893a2474eda2b3665f69381455267
turiyag <turiyag@hotmail.com>
2011-03-03 13:51:06 -0700

    Fixed errors in filterUpload.js Updated imgup.js TODO, change filename of imgup.js to something more reasonable

--- static/fileUpload/filterUpload.js
+++ static/fileUpload/filterUpload.js
@@ -4,7 +4,9 @@ Author: Mitchell Ludwig
 //Filters file uploads to accept only gif, jpg and png images.
 var extArray = new Array("zip", "rar", "js", "html", "htm", "jpg", "jpeg", "png", "gif", "bmp", "txt", "css");
 function LimitAttach(form, file) {
-	if (!file) return; //If there is no file, don't submit anything
+	if (!file) {
+        return; //If there is no file, don't submit anything
+    }
 	
     //Extract the file extension
 	var ext = file.split(".").pop();
@@ -18,7 +20,7 @@ function LimitAttach(form, file) {
 	}
     //Otherwise, pop up a messagebox with a warning and don't submit the form
     alert("Accepted file types are " + extArray.join(", "));
-	}
+	
 }
 
 //Prints the accepted file extensions.
