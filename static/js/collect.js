cb81ed47b760f8e39b8fff4be1f8981d1928e959
kellyjj <kellyjj@zone06ec.(none)>
2011-03-04 10:31:31 -0700

    placed Add task page to main

--- static/js/collect.js
+++ static/js/collect.js
@@ -1,3 +1,5 @@
+//var task = require("task");
+
 function initialize() {
 	document.title = "Add A Task";
 	document.getElementById("low").checked=true;
@@ -5,18 +7,20 @@ function initialize() {
 
 function clicked() {
 
-	for(i = 2; i< 5; i++){
+	for(i = 2; i< 6; i++){
 		if(document.getElementById("phrase"+i).value == ""){
 			window.alert("Not all required fields are properly filled");
 			return;
 		}
 	}
 
-	var name = "J Doe";
-	var desc = document.getElementById("phrase2");
-	var ETR = document.getElementById("phrase3");
-	var ETL = document.getElementById("phrase4");
-	var com = document.getElementById("phrase5");
+	var nom = "J Doe";
+	var tnom = document.getElementById("phrase2");
+	var desc = document.getElementById("phrase3");
+	var ETR = document.getElementById("phrase4");
+	var ETL = document.getElementById("phrase5");
+	var com = document.getElementById("phrase6");
+	var date = new Date();
 
 	if(document.getElementById("low").checked){
 		pri = document.getElementById("low").value;
@@ -27,5 +31,6 @@ function clicked() {
 	else if(document.getElementById("high").checked){
 		pri = document.getElementById("high").value;
 	}
-	window.alert("success");
+	//t = new Task(tnom, desc, pri, "open", nom, date);
+	window.alert("<<success>> " + date);
 }
