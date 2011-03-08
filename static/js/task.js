6e7663f96b4e32f5399b1bc56a3c00dafb47bca8
Chris Nguyen <nguyen__chris@hotmail.com>
2011-03-08 12:37:33 -0700

    Users are no longer forced to call load(). Will be done dynamically.

--- static/js/task.js
+++ static/js/task.js
@@ -1,15 +1,13 @@
 /*
-	Intended Usage:
+	Usage:
 	
-	To retrieve a Task from the database, use:
-		var myTask = loadTask(id);	// where id is the id of the Task you want
-
-	If you manipulate the task and wish to save it back to the database, use:
-		myTask.save();
-
 	To create a new Task, use:
 		var myNewTask = new Task(taskName, description, priority, status, user, date);
 	Note that this task is not in the database yet, call myNewTask.save() to do so.
+
+	If you manipulate the task and wish to save it back to the database, use:
+		myTask.save();
+	again.
 */
 
 
@@ -49,14 +47,6 @@ exports.Task = function(taskName, description, priority, status, user, date) {
 	this.save = save;
 }
 
-function loadTask(id) {
-	/* 
-		TODO: Reference the database with id to see if the task already exists.
-		If it does, load the values from the database, create a Task object using 
-			the constructor, and return a reference to this object.
-		Else, return an error/exception of some sort.
-	*/
-}
 
 function setTaskName(newTaskName) {
 	if( newTaskName == null || newTaskName.trim().length == 0 ) {
