62d2dc83e6aa3bb3715aaca0eccc651e65d677f8
turiyag <turiyag@hotmail.com>
2011-03-08 19:08:21 -0700

    Added example for how to edit a page live, using jquery.

---
+++ dynamic/veryDynamic/example.js
@@ -0,0 +1,17 @@
+var pagemaker = require('pagemaker');
+var url = require('url');
+var upops = require('uploadOps');
+
+exports.getReq = function (request,response) {
+    send(response,upops.parseURLEncoded(request.url));
+}
+
+send = function (response, parameters) {
+	response.writeHead(200, {'Content-Type': 'text/html'});
+    if (parameters['attack']=="win") {
+        response.write('<a href="http://en.wikipedia.org/wiki/Win">VICTORY!</a><br /><img src="riverTam.png" />');
+    } else {
+        response.write("DEFEAT! You must win to acheive victory, and you tried to " + parameters['attack']);
+    }
+	response.end();
+}
