694a54d5273b72987d05265987878fc3098359b0
Chris Nguyen <nguyen__chris@hotmail.com>
2011-03-16 14:18:13 -0600

    decided to store date attribute as text

--- js/createDatabase.js
+++ js/createDatabase.js
@@ -47,7 +47,7 @@ db.open(dbLocation, function (error) {
 		"priority TEXT," +
 		"status TEXT," +
 		"user TEXT," +
-		"date NUMBER)",
+		"date TEXT)",
 		function (error) {
 			if(error) {
 				console.log("Error creating task table.");
