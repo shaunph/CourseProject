3ad6b7807026f38be9750302eea8be78ae6795c5
Chris Nguyen <nguyen__chris@hotmail.com>
2011-03-18 09:25:30 -0600

    added error checking and removed date parameter

--- static/js/task.js
+++ static/js/task.js
@@ -14,26 +14,28 @@
 /* Create a task object.
 	
 	taskName 	- Name of the task (String)
-	description - Description of the task (String)
+	description - Description of the task. Pass in an empty string for a Task without a description. (String)
 	priority	- Priority in relation to other tasks (["Low", "Medium", "High"])
-	status		- Current completion status (["Not started", "Work in progress", "Completed"])
-	user		- The creator of the task (String)
-	date		- Date when task was created ( Date(year, month, day, hours, minutes, seconds) )
+	progress	- User defined message of the current progress (String)
+	status		- Availability of task (["Open", "Closed"])
+	user		- The email of the task creator (String)
 */
-exports.Task = function(taskName, description, priority, status, user, date) {
+exports.Task = function(taskName, description, priority, progress, status, user) {
 
-	this.taskName = taskName;
+	this.taskName;
 	this.id = undefined;
-	this.description = description;
-	this.priority = priority;
-	this.status = status;
-	this.user = user;
-	this.date = date;
-
+	this.description;
+	this.priority;
+	this.progress;
+	this.status;
+	this.user;
+	this.date = new Date();	// Date of when the task was created
+	
 	this.getTaskName = function() { return this.taskName; }
 	this.getId = function() { return this.id; }
 	this.getDescription = function() { return this.description; }
 	this.getPriority = function() { return this.priority; }
+	this.getProgress = function() { return this.progress; }
 	this.getStatus = function() { return this.status; }
 	this.getUser = function() { return this.user; }
 	this.getDate = function() { return this.date.toLocaleDateString(); }
@@ -41,51 +43,84 @@ exports.Task = function(taskName, description, priority, status, user, date) {
 	this.setTaskName = setTaskName;
 	this.setDescription = setDescription;
 	this.setPriority = setPriority;
+	this.setProgress = setProgress;
 	this.setStatus = setStatus;
+	this.setUser = setUser;
 
+	// Set properties using set methods to ensure valid parameters.
+	this.setTaskName(taskName);
+	this.setDescription(description);
+	this.setPriority(priority);
+	this.setProgress(progress);
+	this.setStatus(status);
+	this.setUser(user);
+	
 	this.modifyTask = modifyTask;
 	this.save = save;
 }
 
 function setTaskName(newTaskName) {
-	if( newTaskName == null || newTaskName.trim().length == 0 ) {
-		throw new TypeError("No task name provided.");
+	if( typeof(newTaskName) != "string" && typeof(newTaskName) != "number" ) {
+		throw new TypeError("Task name not provided, or invalid Task name type.");
+	}
+	else if ((""+newTaskName).trim().length == 0) {
+		throw new TypeError("Task names must be at least 1 character long.");
 	}
 	this.taskName = newTaskName;
 }
 
 function setDescription(newDescription) {
-	if( newDescription == null || newDescription.trim().length == 0 ) {
-		throw new TypeError("No description provided.");
+	if ( typeof(newDescription) != "string" && typeof(newDescription) != "number" ) {
+		throw new TypeError("Invalid task description type.");
 	}
 	this.description = newDescription;
 }
 
 function setPriority(newPriority) {
-	if( newPriority == null || newPriority.trim().length == 0 ) {
-		throw new TypeError("No priority provided.");
+	if( newPriority != "Low" && newPriority != "Medium" && newPriority != "High" ) {
+		throw new TypeError("Invalid Priority.");
 	}
 	this.priority = newPriority;
 }
 
+function setProgress(newProgress) {
+	if ( typeof(newProgress) != "string" && typeof(newProgress) != "number" ) {
+		throw new TypeError("Invalid task progress type.");
+	}
+	this.progress = newProgress;
+}
+
 function setStatus(newStatus) {
-	if( newStatus == null || newStatus.trim().length == 0 ) {
-		throw new TypeError("No status provided.");
+	if( newStatus != "Open" && newStatus != "Closed" ) {
+		throw new TypeError("Invalid Status.");
 	}
 	this.status = newStatus;
 }
 
-function modifyTask(taskName, description, priority, status) {
+function setUser(newUser) {
+	//TODO: Check if user is a valid email address (of type String)
+	if (typeof(newUser) != "string") {
+		throw new TypeError("Invalid User type.");
+	}
+	else if (newUser.trim().length == 0) {
+		throw new TypeError("User names must be at least 1 character long.");
+	}
+	this.user = newUser;
+}
+
+function modifyTask(taskName, description, priority, progress, status, user) {
 	try {
 		this.setTaskName(taskName);
 		this.setDescription(description);
 		this.setPriority(priority);
+		this.setProgress(progress);
 		this.setStatus(status);
+		this.setUser(user);
 	} catch (e) {
 		if (e instanceof TypeError)
-			alert(e.name + ": " + e.message);
+			throw e;
 		else 
-			alert(e.name + ": " + "Unknown error in modifyTask");
+			throw new Error(e.name + ": " + "Unknown error in modifyTask");
 	}
 }
 
