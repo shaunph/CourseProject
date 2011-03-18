753210d635a8f6bbaef51aa28139eaaedc2eec75
Adam Kromm <adamkromm@gmail.com>
2011-03-18 15:07:50 -0600

    Fixed the signupRequest.js so that it correctly check for an existing user/email and nickname
    made a small change in sqlitehelper.js so that the error codes where a bit more specific.

--- dynamic/signupRequest.js
+++ dynamic/signupRequest.js
@@ -16,20 +16,23 @@ exports.postReq = function(req, res) {
 		
 		db.addUser(fields.Email, fields.Username, fields.Password, function(codes) {
 			res.writeHead(200, {'content-type':'text/html'});
+			page1 = new StandardPage();
+			page1.standardMenus();
 			if(codes.status == 0) {
-					
-				page1 = new pagemaker.StandardPage();
 				page1.setTitle("Success");
 				page1.setContent("Signup successfull! <br />");
-				page1.standardMenus();
-				res.write(page1.toHTML());
-				res.end();
 			}
-			if(codes.status == -2) {
-				res.write(codes.detail);
-				res.end();
+			else if(codes.status == -1) {
+				page1.setTitle("Error");
+				page1.setContent("User already exists in DB");
 			}
-
+			else if(codes.status == -2) {
+				page1.setTitle("Error");
+				page1.setContent("Internal error while signing up");
+			}
+			
+			res.write(page1.toHTML());
+			res.end();
 		});	
 	});
 }
