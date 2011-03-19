c7e85820f2207fc475d0fab462541bc78b5dde6f
Chris Nguyen <nguyen__chris@hotmail.com>
2011-03-18 21:01:13 -0600

    removed <br /> tags, fixed relative paths, modified parameters to loadedTask and testTask

--- dynamic/taskpage.js
+++ dynamic/taskpage.js
@@ -1,6 +1,6 @@
 var dbHelper = require('./../js/SQLiteHelper'),
-	task = require('./../static/js/task'),
-	pagemaker = require('./../js/pagemaker'),
+	task = require('./../node_modules/task').task,
+	pagemaker = require('./../node_modules/pagemaker'),
 	url = require('url');
 
 // Important Note: Until a save function is implemented, the temporary saveTestTask() 
@@ -18,33 +18,33 @@ var dbHelper = require('./../js/SQLiteHelper'),
 //		5) Connect to the server and click on Task page 
 
 exports.getReq = function (request,response) {
-	saveTestTask();	// This will be removed once tasks can be saved
+	//saveTestTask();	// This will be removed once tasks can be saved
 
 	var params = url.parse(request.url).query;
 	var index = params.indexOf('=');
-	//loadTask(response, parseInt(params.substring(index+1)));
+	loadTask(response, parseInt(params.substring(index+1)));
 }
 
 function displayTaskPage(response, id, taskValues) {
 	var taskPage = new StandardPage();
 	taskPage.setTitle(taskValues.getTaskName() + " Details");
 	
-	taskPage.setContent("<h3> Description <br /> </h3>");
+	taskPage.setContent("<h3> Description </h3>");
 	taskPage.addContent(taskValues.getDescription());
 	
-	taskPage.addContent("<h3> <br /> Priority <br /> </h3>");
+	taskPage.addContent("<h3> Priority </h3>");
 	taskPage.addContent(taskValues.getPriority());
 
-	taskPage.addContent("<h3> <br /> Progress <br /> </h3>");
+	taskPage.addContent("<h3> Progress  </h3>");
 	taskPage.addContent(taskValues.getProgress());
 	
-	taskPage.addContent("<h3> <br /> Status <br /> </h3>");
+	taskPage.addContent("<h3> Status </h3>");
 	taskPage.addContent(taskValues.getStatus());
 
-	taskPage.addContent("<h3> <br /> User <br /> </h3>");
+	taskPage.addContent("<h3> User </h3>");
 	taskPage.addContent(taskValues.getUser());
 	
-	// TODO : Disable this link if task is marked as closed
+	// TODO : Disable this link if task status is marked as closed
 	taskPage.addMenuItem("Update Task", "/updatetask.html");
 	taskPage.addMenuItem("home", "/index.html");
 
@@ -56,14 +56,15 @@ function displayTaskPage(response, id, taskValues) {
 function loadTask(response, id) {
 	dbHelper.getTask(id, function(callbackObj) {
 			var loadRow = callbackObj.rows[0];	// Always 0 because getTask only gets 1 row, namely the row with taskid = id
-			var loadedTask = new task.Task(loadRow.taskName, loadRow.description, loadRow.priority, loadRow.progress, loadRow.status, loadRow.user);
+			var loadedTask = new task(loadRow.taskName, loadRow.description, loadRow.timeSpent, loadRow.timeLeft, 
+											loadRow.priority, loadRow.progress, loadRow.status, loadRow.user);
 			displayTaskPage(response, id, loadedTask);
 	});
 }
 
 // Saves a test task to the DB. Will be removed once tasks can be saved.
 function saveTestTask() {
-	var testTask = new task.Task("test name", "a description", "High", "Not started", "Open", "test@test.com");
+	var testTask = new task("test name", "a description", "0", "0", "High", "Not started", "Open", "test@test.com");
 
 	dbHelper.addTask(testTask, function(error) {});
 }
