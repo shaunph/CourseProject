93fb70185fd898224a05281e7aba89f9a8f40616
Chris Nguyen <nguyen__chris@hotmail.com>
2011-03-18 20:06:06 -0600

    updated task table with task estimation parameters

--- js/createDatabase.js
+++ js/createDatabase.js
@@ -44,8 +44,8 @@ db.open(dbLocation, function (error) {
 		"taskid INTEGER PRIMARY KEY AUTOINCREMENT," +
 		"taskName TEXT," +
 		"description TEXT," +
-		"timeLeft TEXT," +
 		"timeSpent TEXT," +
+		"timeLeft TEXT," +
 		"priority TEXT," +
 		"progress TEXT," +
 		"status TEXT," +
