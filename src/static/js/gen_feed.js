84f81980b97252f6a354c40fa443415b995b452a
Adam Kromm <adamkromm@gmail.com>
2011-03-24 20:21:16 -0600

    fixed gen_feed so that used absolute rather than relative url. now the feeds show up on every page.

--- src/static/js/gen_feed.js
+++ src/static/js/gen_feed.js
@@ -1,10 +1,10 @@
 // Uses JQuery to pull the feed data and place it inside the div
 
 // pullGenFeedsTo(divID)
-//	gets the feed data from the server and sets the content 
-//	of the given divID to the feed data
+//  gets the feed data from the server and sets the content 
+//  of the given divID to the feed data
 function pullGenFeedsTo(divID){
-	$.get('feedGen?', function(data) {
-		$("div.#"+divID).html("<h1>Feeds</h1>" + data);
-	});
+    $.get('/feedGen?', function(data) {
+        $("div.#"+divID).html("<h1>Feeds</h1>" + data);
+    });
 }
