da18d2dee3c7a30f683e85872bf68512e4722a00
Cindy <cxyloo@ucalgary.ca>
2011-04-01 10:40:27 -0600

    added update.js; fixed updateTask in SQLiteHelper.js; edited updatetask.js

---
+++ src/dynamic/update.js
@@ -0,0 +1,51 @@
+var db = require('SQLiteHelper'),
+    task = require('task').task,
+    querystring = require('querystring'),
+    pagemaker = require('pagemaker');
+
+/*
+ * Takes a post request and store the data in buffer.
+ * Parses the buffer into results.
+ */
+exports.postReq = function(request, response) {
+    var buffer, results;
+
+    request.on('data', function(data) {
+        buffer += data;
+    });
+    request.on('end', function() {
+        results = querystring.parse(buffer);
+        if (results.undefinedid) {
+            results.id = results.undefinedid;
+        }
+        result(response, results);
+    });
+}
+
+/*
+ * Updates the specified task and displays a message.
+ */
+result = function(response, results) {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+
+    page = new StandardPage();
+    page.setTitle("Update Results");
+    page.setContent("");
+
+    try {
+        db.updateTask(results.id, results.name, results.description, results.level, results.status, function(callback) {});
+        page.addContent("Task was updated successfully.<br />");
+    } catch(e) {
+        page.addContent("Error occurred. Could not update task.<br />");
+    }
+
+    var id = results.id;
+
+    // Specified link temporary; to be changed
+    page.addContent("<a href='#'>Return to Task List page.</a>");
+
+    page.standardMenus();
+
+    response.write(page.toHTML());
+    response.end();
+}
--- src/dynamic/updatetask.js
+++ src/dynamic/updatetask.js
@@ -1,90 +1,107 @@
-var	//db = require('SQLiteHelper'),
-	task = require('task'),
-	pagemaker = require('pagemaker'),
-	url = require('url');
+var db = require('SQLiteHelper'),
+    task = require('task').task,
+    pagemaker = require('pagemaker'),
+    url = require('url');
 
 exports.getReq = function(request, response) {
-	test(response);
-	//loadTask(response, url.parse(request.url,true).query);
+    loadTask(response, url.parse(request.url,true).query);
 }
 
 /* 
  * Takes in response and a task object as parameters.
  * Defines the response to be an html page and displays a form with fields where the task object's attributes are displayed.
  */
-function displayUpdate(response, taskObj) {
-	response.writeHead(200, {'Content-Type': 'text/html'});
+function displayUpdate(response, id, taskObj) {
+    response.writeHead(200, {'Content-Type': 'text/html'});
 
-	page1 = new StandardPage();
-	page1.setTitle("Update Task");
-	
-	page1.setContent("Id: " + taskObj.getId() + "<br /><br />");
-	page1.addContent("User: " + taskObj.getUser() + "<br /><br />");
-	page1.addContent("Date: " + taskObj.getDate() + "<br /><br />");
-	
-	page1.addContent(
-		"<form method=get action='update.js'>"+
-			
-			"<input type='hidden' name='id' value='"+taskObj.getId()+"'>"+
-			
-			"Name: "+
-			"<input type='text' name='name' onclick='this.select()' value='"+taskObj.getTaskName()+"'><br /><br />"+
-			
-			"Status: "+
-			"<input type='text' name='status' onclick='this.select()' value='"+taskObj.getStatus()+"'><br /><br />"+
-			
-			"Description:<br />"+
-			"<textarea name='description' onclick='this.select()' rows='10' cols='85'>"+taskObj.getDescription()+"</textarea><br /><br />"+
-			
-			"Priority:"+
-			"<input type='radio' name='level' value='low'"+checkPriority(taskObj,'low')+">Low "+
-			"<input type='radio' name='level' value='medium'"+checkPriority(taskObj,'medium')+">Medium "+
-			"<input type='radio' name='level' value='high'"+checkPriority(taskObj,'high')+">High <br /><br />"+
-			
-			"<input type='submit' name='changes' value='Submit Changes'>"+
-			"<input type='button' name='' value='Change Estimate Values' onclick='parent.location=\'/\''>"+
-			"<input type='button' value='Reset' onclick='history.go(0)'>"+
-			"<input type='button' value='Go Back' onclick='history.go(-1);return true;'>"+
+    page = new StandardPage();
+    page.setTitle("Update Task");
 
-		"</form>"
-	);
-	
-	page1.addContent(
-		"<form method=post name=upform action='fileUpload/uploadFile.js' enctype='multipart/form-data'>"+
-			"Attachment:"+
+    page.setContent("User: " + taskObj.getUser() + "<br /><br />");
+    page.addContent("Date: " + taskObj.getDate() + "<br /><br />");
+
+    page.addContent(
+        "<form method=post action='update'>"+
+
+            "<input type='hidden' name='id' value='"+id+"'>"+
+
+            "Name: "+
+            "<input type='text' name='name' onclick='this.select()' value='"+taskObj.getTaskName()+"'><br /><br />"+
+
+            "Status: "+
+            "<input type='text' name='status' onclick='this.select()' value='"+taskObj.getStatus()+"'><br /><br />"+
+
+            "Progress: "+
+            "<input type='text' name='progress' onclick='this.select()' value='"+taskObj.getProgress()+"'><br /><br />"+
+
+            "Description:<br />"+
+            "<textarea name='description' onclick='this.select()' rows='10' cols='85'>"+taskObj.getDescription()+"</textarea><br /><br />"+
+
+            "Priority:"+
+            "<input type='radio' name='level' value='Low'"+checkPriority(taskObj,'Low')+">Low "+
+            "<input type='radio' name='level' value='Medium'"+checkPriority(taskObj,'Medium')+">Medium "+
+            "<input type='radio' name='level' value='High'"+checkPriority(taskObj,'High')+">High <br /><br />"+
+
+            "<input type='submit' name='changes' value='Submit Changes'>"+
+            "<input type='button' name='' value='Change Estimate Values' onclick='parent.location=\'/\''>"+
+            "<input type='button' value='Reset' onclick='history.go(0)'>"+
+            "<input type='button' value='Go Back' onclick='history.go(-1);return true;'>"+
+
+        "</form>"
+    );
+
+    page.addContent(
+        "<form method=post name=upform action='fileUpload/uploadFile' enctype='multipart/form-data'>"+
+            "Attachment:"+
             "<input type='hidden' name='MAX_FILE_SIZE' value='500' />"+
             "<input type=file name=uploadfile>"+
-			"<br />"+
-			
-			"<input type='button' name='Submit' value='Upload Attachment' onclick='LimitAttach(this.form, this.form.uploadfile.value)'>"+
-		"</form>"
-	);
+            "<br />"+
+
+            "<input type='button' name='Submit' value='Upload Attachment' onclick='LimitAttach(this.form, this.form.uploadfile.value)'>"+
+        "</form>"
+    );
 	
-	page1.standardMenus();
-	
-	response.write(page1.toHTML());
-	response.end();
+    page.standardMenus();
+
+    response.write(page.toHTML());
+    response.end();
 }
 
 /* Checks if the current radio button matches the task's priority level. */
 function checkPriority(taskObj, current) {
-	if (taskObj.getPriority() == current)
-		return " checked ";
+    if (taskObj.getPriority() == current)
+        return " checked ";
 }
 
 /*
  * Loads a task
  */
 function loadTask(response, param){
-	db.getTask(param["id"], function(callback){
-		var row = callback.row[0];
-		var loadedTask = new task.Task(row.taskName, row.description, row.timeSpent, row.timeLeft, row.priority, row.status, row.user, row.date);
-		displayUpdate(response, loadedTask);
-	});
+    db.getTask(param["id"], function(callback){
+        try {
+            var row = callback.rows[0];
+            var loadedTask = new task(row.taskName, row.description, row.timeSpent, row.timeLeft, row.priority, row.progress, row.status, row.user);
+            displayUpdate(response, param["id"], loadedTask);
+        } catch (e) {
+			message(response);
+		}
+    });
 }
 
-/* Test coding for a task object */
-function test(response) {
-	var taskObj = new task.task("taskName", "desc", "0", "0", "low", "status", "user", "date");
-	displayUpdate(response, taskObj);
+/*
+ * Sends a temporary page that indicates what's wrong
+ */
+message = function(response) {
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+
+    page = new StandardPage();
+    page.setTitle("Update Task");
+
+    page.setContent("Please indicate a valid id.");
+    page.standardMenus();
+
+    response.write(page.toHTML());
+    response.end();
 }
