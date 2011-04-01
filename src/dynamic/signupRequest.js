63adba6c9b9870d52770bf4470bd933b016a33e8
Chad Morrison <Chad.p.morrison@gmail.com>
2011-04-01 00:52:26 -0600

    Clean up some comments that were left in

--- src/dynamic/signupRequest.js
+++ src/dynamic/signupRequest.js
@@ -5,39 +5,26 @@ var bops = require('bufferOps.js');
 var pagemaker = require('pagemaker');
 
 exports.postReq = function(req, res, dataBuffer) {
-
-    /*var body = '';
-    var fields = {};
-
-    req.on('data', function(data) {
-        body += data;
-    });
-
-    req.on('end', function () {
-        fields = qs.parse(body);*/
-        console.log("SIGN: " + dataBuffer);
-        var parsed = upops.parseMultipartFormdata(dataBuffer);
-        console.log("SIGN: " + parsed);
+    var parsed = upops.parseMultipartFormdata(dataBuffer);
+    
+    db.addUser(parsed["Email"].toString(), parsed["Username"].toString(), parsed["Password"].toString(), function(codes) {
+        res.writeHead(200, {'content-type':'text/html'});
+        page1 = new StandardPage();
+        page1.standardMenus();
+        if(codes.status == 0) {
+            page1.setTitle("Success");
+            page1.setContent("<h1>Signup successfull!</h1> <br />");
+        }
+        else if(codes.status == -1) {
+            page1.setTitle("Error");
+            page1.setContent("<h1>User already exists in DB</h1>");
+        }
+        else if(codes.status == -2) {
+            page1.setTitle("Error");
+            page1.setContent("<h1>Internal error while signing up</h1>");
+        }
         
-        db.addUser(parsed["Email"].toString(), parsed["Username"].toString(), parsed["Password"].toString(), function(codes) {
-            res.writeHead(200, {'content-type':'text/html'});
-            page1 = new StandardPage();
-            page1.standardMenus();
-            if(codes.status == 0) {
-                page1.setTitle("Success");
-                page1.setContent("<h1>Signup successfull!</h1> <br />");
-            }
-            else if(codes.status == -1) {
-                page1.setTitle("Error");
-                page1.setContent("<h1>User already exists in DB</h1>");
-            }
-            else if(codes.status == -2) {
-                page1.setTitle("Error");
-                page1.setContent("<h1>Internal error while signing up</h1>");
-            }
-            
-            res.write(page1.toHTML());
-            res.end();
-        });    
-    //});
+        res.write(page1.toHTML());
+        res.end();
+    });    
 }
