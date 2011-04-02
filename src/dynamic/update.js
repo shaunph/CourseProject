7e78ead58364d00915ef51f9fa01a927742c5934
Cindy <cxyloo@ucalgary.ca>
2011-04-01 18:43:49 -0600

    fixed other page

--- src/dynamic/update.js
+++ src/dynamic/update.js
@@ -1,24 +1,14 @@
 var db = require('SQLiteHelper'),
-    querystring = require('querystring'),
+    upops = require('uploadOps'),
     pagemaker = require('pagemaker');
 
 /*
  * Takes a post request and store the data in buffer.
  * Parses the buffer into results.
  */
-exports.postReq = function(request, response) {
-    var buffer, results;
-
-    request.on('data', function(data) {
-        buffer += data;
-    });
-    request.on('end', function() {
-        results = querystring.parse(buffer);
-        if (results.undefinedid) {
-            results.id = results.undefinedid;
-        }
-        result(response, results);
-    });
+exports.postReq = function(request, response, dataBuffer) {
+    var results = upops.parseMultipartFormdata(dataBuffer);
+    result(response, results);
 }
 
 /*
@@ -30,16 +20,13 @@ result = function(response, results) {
     page = new StandardPage();
     page.setTitle("Update Results");
     page.setContent("");
-console.log(results.id + " " + results.description + " " + results.level + " " + results.progress + " " + results.status);
-console.log(results);
+
     try {
-        db.updateTask(results.id, results.description, results.level, results.progress, results.status, function(callback) {});
+        db.updateTask(results["id"].toString(), results["description"].toString(), results["level"].toString(), results["progress"].toString(), results["status"].toString(), function(callback) {});
         page.addContent("Task was updated successfully.<br />");
     } catch(e) {
         page.addContent("Error occurred. Could not update task.<br />");
     }
-
-    var id = results.id;
 
     // Specified link temporary; to be changed
     page.addContent("<a href='#'>Return to Task List page.</a>");
