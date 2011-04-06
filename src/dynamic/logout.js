c2280ee2c6abf232fa5233eef9e08ddef3e3e918
Adam Kromm <adamkromm@gmail.com>
2011-04-06 16:44:27 -0600

    logout page fixed so that it works correctly.

--- src/dynamic/logout.js
+++ src/dynamic/logout.js
@@ -2,14 +2,18 @@ var upops = require('uploadOps');
 
 module.exports.getReq = function(req, res) {
 
-    var cookies = upops.parseCookies(req.headers);
+    try {
+        var cookies = upops.parseCookies(req.headers);
+        var page = new StandardPage();
+        page.standardMenus();
+        page.setTitle("Logout");
+        page.setContent("<h1>Logout successfull</h1>");
 
-    var page = new StandardPage(req);
-    page.standardMenus();
-    page.setContent("<h1>Logout successfull</h1>");
-
-    res.writeHead(200, {    'Content-Type': 'text/html', 
-                            'Set-Cookie':   'Email=' + cookies.Email + '; expires=Thu, 01-Jan-1970 00:00:01 GMT;' + ";" +
-                                            'Nickname=' + cookies.Nickname + '; expires=Thu, 01-Jan-1970 00:00:01 GMT;'});
-    res.end(page.toHTML());
+        res.writeHead(200, {    'Content-Type': 'text/html', 
+                                'Set-Cookie':   'Email=' + cookies.Email + '; expires=Thu, 01-Jan-1970 00:00:01 GMT;' + ";" +
+                                                'Nickname=' + cookies.Nickname + '; expires=Thu, 01-Jan-1970 00:00:01 GMT;'});
+        res.end(page.toHTML());
+    } catch(err) {
+        console.log("There was an error: " + err);
+    } 
 }
