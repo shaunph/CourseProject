9345a54122c7560248a5465ce1687f0acf02f5fa
Adam Kromm <adamkromm@gmail.com>
2011-04-06 16:33:39 -0600

    added files for user authentication added login page added changes to pagemaker added logout page

---
+++ src/dynamic/checkCredentials.js
@@ -0,0 +1,63 @@
+var db = require('SQLiteHelper');
+var qs = require('querystring');
+var upops = require('uploadOps');
+var pagemaker = require('pagemaker');
+
+module.exports.postReq = function(request, response, dataBuffer) {
+
+    try {
+        
+        var parsed = upops.parseMultipartFormdata(dataBuffer);
+        
+        db.userExists(parsed["Email"].toString(), function(codes) {
+            if(codes.status == 0 && codes.exists == true) {
+                db.getUser(parsed["Email"].toString(), function(codes) {
+        
+                    //right here we need to make sure the passwords match before success screen
+                    if(parsed["Password"].toString() == codes.rows[0].password) {
+                        //success
+                        response.writeHead(200, {   'content-type':'text/html', 
+                                                    'set-cookie':   'Email='+codes.rows[0].email + " ; ;" +
+                                                                    'Nickname='+codes.rows[0].nickname
+                                            });
+                        var page = new StandardPage(request);
+                        page.setTitle("Login Successful");
+                        page.setContent("<h1>User found</h1>");
+                        page.standardMenus();
+                        response.end(page.toHTML());
+                    }
+                    else {
+                        //failure
+                        response.writeHead(200, { 'content-type':'text/html' });
+                        var page = new StandardPage(request);
+                        page.setTitle("Login Denied");
+                        page.setContent("<h1> Denied </h1>");
+                        page.addContent("<h2> Username and password don't match </h2>");
+                        page.standardMenus();
+                        response.end(page.toHTML());
+                    }
+              });
+            }
+            else if(codes.status == 0 && codes.exists == false) {
+                response.writeHead(200, {'content-type':'text/html'});
+                var page = new StandardPage(request);
+                page.setTitle("Login");
+                page.setContent("<h1>User not found</h1>");
+                page.standardMenus();
+                response.end(page.toHTML());
+            }
+            else {
+                response.writeHead(200, {'content-type':'text/html'});
+                var page = new StandardPage(request);
+                page.setTitle("Login - Error");
+                page.setContent("<h1>There was a server error</h1>");
+                page.standardMenus();
+                response.end(page.toHTML());               
+            }
+        });
+
+    } catch(err) {
+        console.log(err);
+    }
+}
+
