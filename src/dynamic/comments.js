19bcdf6c21b7977bb17434f8c9f52c6a385f8803
Chad Morrison <Chad.p.morrison@gmail.com>
2011-04-12 11:29:23 -0600

    update add comment to check for and use cookies

--- src/dynamic/comments.js
+++ src/dynamic/comments.js
@@ -22,8 +22,6 @@ exports.postReq = function (request,response, dataBuffer) {
             send(response, request, url.parse(request.url,true).query["taskid"]);
         }
     });
-
-
 }
 
 send = function (response,request, taskId) {
@@ -51,18 +49,27 @@ send = function (response,request, taskId) {
 
             var page = new StandardPage(request);
             page.setTitle("Comments for task " + taskId);
-            
-            // Add comment form
-            page.addContent("<div><span>Enter a comment:</span></div>" +
-                            "<br />");
-            
-            page.addContent("<form action='comments?taskid=" + taskId + "' method='post' enctype='multipart/form-data'>" + 
-                "<textarea name='thecomment' rows='4' cols='50'></textarea><br />" +
-                "<input type='hidden' name='taskid' value='" + taskId + "'>" + 
-		"<input type='hidden' name='email' value='1@1.1'>" + 
-                "<input type='submit' value='Save'>" +
-                "</form>");
-            
+
+            var cookies = upops.parseCookies(request.headers);
+           
+            // Show add comement box if user is logged in 
+            if(cookies.Email) {
+                // Add comment form
+                page.addContent("<div><span>Enter a comment:</span></div>" +
+                        "<br />");
+
+                page.addContent("<form action='comments?taskid=" + taskId + "' method='post' enctype='multipart/form-data'>" + 
+                        "<textarea name='thecomment' rows='4' cols='50'></textarea><br />" +
+                        "<input type='hidden' name='taskid' value='" + taskId + "'>" + 
+                        "<input type='hidden' name='email' value='"+cookies.Email+"'>" + 
+                        "<input type='submit' value='Save'>" +
+                        "</form>");
+            }
+            else {
+                page.addContent("<div><span style='font-weight:bold'>Login to add a comment.</span></div>" + 
+                    "<br />");
+            }
+
             var numComments = obj.rows.length;
             
             // If there are no comments for the task, say so
@@ -83,13 +90,8 @@ send = function (response,request, taskId) {
             }
             
             page.standardMenus();
-
             response.write(page.toHTML());
-            
             response.end();
-            
         });
-    
     }
-    
 }
