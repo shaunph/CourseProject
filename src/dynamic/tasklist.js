caf78a2350272baccee8fa0a3e7a0405e92d171e
Kevin <kevin@ubuntu.(none)>
2011-04-12 18:36:04 -0600

    Really added a task list page this time.

---
+++ src/dynamic/tasklist.js
@@ -0,0 +1,107 @@
+
+url = require('url');
+slh = require('SQLiteHelper');
+pagemaker = require('pagemaker');
+
+
+exports.getReq = function(request, response) {
+
+    var parameters = url.parse(request.url,true).query;
+    loadTaskList(parameters, response);
+}
+
+function loadTaskList(parameters, response) {
+
+    response.writeHead(200, {'Content-Type' : 'text/html'});
+
+    console.log("Getting tasks table...");
+
+    slh.getTable("task", function(obj) {
+        if(obj.status != 0) {
+            console.log("error getting tasks table: " + obj.detail);
+            return;
+        }
+
+        console.log("Tasks Table retrieved.");
+
+        var taskListPage = "";
+        taskListPage = taskListPage.concat("<h4>" +
+                "<div class='row'>" +
+                "<div class='taskListHeading'>Name</div>" +
+                "<div class='taskListHeading'>Priority</div>" +
+                "<div class='taskListHeading'>Progress</div>" +
+                "<div class='taskListHeading'>Status</div>" +
+                "<div class='taskListHeading'>Creator</div>" +
+                "</h4></div>");
+
+        for(i = 0; i < obj.rows.length; i++) {
+	
+                rowID = obj.rows[i].taskid;
+                rowName = obj.rows[i].taskName;
+                rowPriority = obj.rows[i].priority;
+                rowProgress = obj.rows[i].progress;
+                rowStatus = obj.rows[i].status;
+                rowUser = obj.rows[i].user;
+                //rowDesc = obj.rows[i].description;
+                if(rowPriority == "High") 
+                {
+                        taskListPage = taskListPage.concat("<div class='row'>" +
+                        "<div class='taskOutput'><a href=\"task?id=" + rowID + "\">"+ rowName + "</a></div>" +
+                        "<div class='taskOutput'>" + rowPriority + "</div>" +
+                        "<div class='taskOutput'>" + rowProgress + "</div>" +
+                        "<div class='taskOutput'>" + rowStatus + "</div>" +
+                        "<div class='taskOutput'>" + rowUser + "</div>" +
+                        "</div>");
+                }
+
+        }
+
+        for(i = 0; i < obj.rows.length; i++) {
+	
+                rowID = obj.rows[i].taskid;
+                rowName = obj.rows[i].taskName;
+                rowPriority = obj.rows[i].priority;
+                rowProgress = obj.rows[i].progress;
+                rowStatus = obj.rows[i].status;
+                rowUser = obj.rows[i].user;
+                //rowDesc = obj.rows[i].description;
+                if(rowPriority == "Medium") 
+                {
+                        taskListPage = taskListPage.concat("<div class='row'>" +
+                        "<div class='taskOutput'><a href=\"task?id=" + rowID + "\">"+ rowName + "</a></div>" +
+                        "<div class='taskOutput'>" + rowPriority + "</div>" +
+                        "<div class='taskOutput'>" + rowProgress + "</div>" +
+                        "<div class='taskOutput'>" + rowStatus + "</div>" +
+                        "<div class='taskOutput'>" + rowUser + "</div>" +
+                        "</div>");
+                }
+
+        }
+        for(i = 0; i < obj.rows.length; i++) {
+	
+                rowID = obj.rows[i].taskid;
+                rowName = obj.rows[i].taskName;
+                rowPriority = obj.rows[i].priority;
+                rowProgress = obj.rows[i].progress;
+                rowStatus = obj.rows[i].status;
+                rowUser = obj.rows[i].user;
+                //rowDesc = obj.rows[i].description;
+                if(rowPriority == "Low") 
+                {
+                        taskListPage = taskListPage.concat("<div class='row'>" +
+                        "<div class='taskOutput'><a href=\"task?id=" + rowID + "\">"+ rowName + "</a></div>" +
+                        "<div class='taskOutput'>" + rowPriority + "</div>" +
+                        "<div class='taskOutput'>" + rowProgress + "</div>" +
+                        "<div class='taskOutput'>" + rowStatus + "</div>" +
+                        "<div class='taskOutput'>" + rowUser + "</div>" +
+                        "</div>");
+                }
+
+        }
+        response.write(taskListPage);
+        response.end();
+
+});
+
+
+}
